3d9082208cbfdb57ac5350e531d867d6cc7e6bb2
shloksomani <shloksomani@ymail.com>
2020-02-09 14:31:28 -0500

    added the final code for lab 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,14 +11,16 @@
     var $window = $(window),
       $body = $("body");
 
-    // Disable animations/transitions until the page has loaded.
-    // $body.addClass("is-loading");
+    $(".jquery-background-video").bgVideo({ fadeIn: 2000 });
 
-    // $window.on("load", function() {
-    //   window.setTimeout(function() {
-    //     $body.removeClass("is-loading");
-    //   }, 100);
-    // });
+    // Disable animations/transitions until the page has loaded.
+    $body.addClass("is-loading");
+
+    $window.on("load", function() {
+      window.setTimeout(function() {
+        $body.removeClass("is-loading");
+      }, 100);
+    });
 
     // Fix: Placeholder polyfill.
     $("form").placeholder();
@@ -74,29 +76,6 @@
 
     // Scrolly.
     $(".scrolly").scrolly();
-
-    // // Poptrox.
-    // $window.on("load", function() {
-    //   var $thumbs = $(".thumbnails");
-
-    //   if ($thumbs.length > 0)
-    //     $thumbs.poptrox({
-    //       onPopupClose: function() {
-    //         $body.removeClass("is-covered");
-    //       },
-    //       onPopupOpen: function() {
-    //         $body.addClass("is-covered");
-    //       },
-    //       baseZIndex: 10001,
-    //       useBodyOverflow: false,
-    //       overlayColor: "#222226",
-    //       overlayOpacity: 0.75,
-    //       popupLoaderText: "",
-    //       fadeSpeed: 500,
-    //       usePopupDefaultStyling: false,
-    //       windowMargin: skel.breakpoint("small").active ? 5 : 50
-    //     });
-    // });
 
     // Initial scroll.
     $window.on("load", function() {
