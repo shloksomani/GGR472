0716daa50947a8673a153f1f602f7a323fa4e8cc
shloksomani <shloksomani@ymail.com>
2020-02-18 13:33:39 -0500

    changed the pixel count to 50

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,7 +113,7 @@ function smoothScroll(eID) {
   var startY = currentYPosition();
   var stopY = elmYPosition(eID);
   var distance = stopY > startY ? stopY - startY : startY - stopY;
-  if (distance < 100) {
+  if (distance < 50) {
     scrollTo(0, stopY);
     return;
   }
